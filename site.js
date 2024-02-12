8b55371fa8031232cee99a69a43a8436b7d23f5f
Summer Ruff <summerruff13@gmail.com>
2024-02-12 10:39:51 -0600

    added welcome div

---
+++ site.js
@@ -0,0 +1,21 @@
+function welcome(){
+    const hours = new Date().getHours() // get the current hour
+    message = '' //string to be returned
+
+    const isMorning = hours >= 4 && hours < 12 // is it morning?
+    const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+    const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+    if(isMorning){
+        message = 'Good Morning!'
+    }else if(isAfternoon){
+        message = 'Good Afternoon!'
+    }else if(isEvening){
+        message = 'Good Evening!'
+    }
+
+    return message;
+}
+
+h1 = document.querySelector('h1')
+h1.textContent = welcome()
